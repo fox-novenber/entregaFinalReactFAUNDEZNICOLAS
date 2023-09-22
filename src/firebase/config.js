ce73d48139c9c4c915e3b70193f31d5cccd66834
Nicolas Faundez <nicolasfaundez@hotmail.com>
2023-09-22 20:09:34 -0300

    entrega final Faundez Nicolas fb

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDoj0Q4EDPtYq-oFiTdnAMq9kXkpwHeeZk",
+  authDomain: "ecommerce-faundeznicolas.firebaseapp.com",
+  projectId: "ecommerce-faundeznicolas",
+  storageBucket: "ecommerce-faundeznicolas.appspot.com",
+  messagingSenderId: "527442311438",
+  appId: "1:527442311438:web:012071894b9100c873bf5f"
+};
+
+// Initialize Firebase
+initializeApp(firebaseConfig);
